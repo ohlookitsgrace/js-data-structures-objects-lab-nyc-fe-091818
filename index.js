38ddafd73568aaeed355b4a10a48596610e6c9b6
Grace Han <ohlookitsgrace@gmail.com>
2018-10-25 22:47:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,5 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 function deleteFromDriverByKey(driver, key) {
   const driverCopy = {...driver}
   delete driverCopy[key]
+  return driverCopy
 }
