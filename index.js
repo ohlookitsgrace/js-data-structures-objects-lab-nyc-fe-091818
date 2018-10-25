058042de8232917918d3e354368ad3e62d4d850d
Grace Han <ohlookitsgrace@gmail.com>
2018-10-25 22:44:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ function updateDriverWithKeyAndValue (driver, key, value) {
   return driverCopy
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value
+  return driver
+}
