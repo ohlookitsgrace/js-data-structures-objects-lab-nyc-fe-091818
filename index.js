b5bcf60f65cf537523bec4aa2407292b2469e650
Grace Han <ohlookitsgrace@gmail.com>
2018-10-25 22:34:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your solution in this file!
 let driver = {}
 
-
 function updateDriverWithKeyandValue () {
   const driverCopy = Object.assign ({}, driver)
   driverCopy.ability = 'expert'
