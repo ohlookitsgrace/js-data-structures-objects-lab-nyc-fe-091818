baf5054566160d9e9da269431eabd5376d7c0da3
Grace Han <ohlookitsgrace@gmail.com>
2018-10-25 22:42:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let driver = {}
 
 function updateDriverWithKeyAndValue (driver, key, value) {
-  driver[key] = value
-  return driver
+  const driverCopy = {...driver}
+  driverCopy[key] = value
+  return driverCopy
 }
